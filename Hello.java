deac709a587277eba9b76894f805c1126a5477fe
Gokul <pgokulmca@gmail.com>
2023-02-11 22:19:52 +0530

    variables and primitive data type

--- Hello.java
+++ Hello.java
@@ -3,5 +3,32 @@
 class Hello {
     public static void main(String a[]) {
         System.out.println("Gokul....");
+        System.out.println(a);
+
+
+        // Variables
+        byte by = 127;
+        System.out.println(by);
+
+        int inte = 199999999;
+        System.out.println(inte);
+
+        short sh = 128;
+        System.out.println(sh);
+
+        long lo = 199999999312321321l;
+        System.out.println(lo);
+
+        float flo = 1.2f;
+        System.out.println(flo);
+
+        double dou = 1.4;
+        System.out.println(dou);
+
+        char d = 'c';
+        System.out.println(d);
+
+        boolean e = true;
+        System.out.println(e);        
     }
 }
