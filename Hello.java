d3382dce6c02bf0354d5f56fca47602f82ca1d29
Gokul <pgokulmca@gmail.com>
2023-02-11 21:02:25 +0530

    First program

--- Hello.java
+++ Hello.java
@@ -1,1 +1,7 @@
-System.out.println("Gokul.....");
+// try following in "jshell"
+// System.out.println("Gokul.....");
+class Hello {
+    public static void main(String a[]) {
+        System.out.println("Gokul....");
+    }
+}
